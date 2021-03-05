20b4b0e27d462e4ab0b9841713baa31f742eba70
VSKerberos <yunus.erdoagan@gmail..com>
2021-03-05 23:57:18 +0300

    End of Section 15

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import {HttpClient, HttpParams } from '@angular/common/http'
 import { Member  } from '../_models/member';
-import { PaginatedResult  } from '../_models/pagination';
 import { of } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
 import {UserParams} from '../_models/userParams';
 import { AccountService } from './account.service';
 import { take } from 'rxjs/operators';
 import { User } from '../_models/user';
-
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
   providedIn: 'root'
@@ -49,7 +48,7 @@ export class MembersService {
       return of(response);
     }
     
-    let params = this.getPaginationHeaders(userParams.pageNumber,userParams.pageSize);
+    let params = getPaginationHeaders(userParams.pageNumber,userParams.pageSize);
 
     params = params.append('minAge',userParams.minAge.toString());
     params = params.append('maxAge',userParams.maxAge.toString());
@@ -58,7 +57,7 @@ export class MembersService {
 
 
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl +'users',params)
+    return getPaginatedResult<Member[]>(this.baseUrl +'users',params, this.http)
     .pipe(map(response =>{
       this.memberCache.set(Object.values(userParams).join('-'),response);
         return response;
@@ -103,32 +102,10 @@ export class MembersService {
 
 
    getLikes(predicate: string, pageNumber,pageSize){
-    let params = this.getPaginationHeaders(pageNumber,pageSize);
+    let params =getPaginationHeaders(pageNumber,pageSize);
     params = params.append('predicate',predicate);
-    return this.getPaginatedResult<Partial<Member[]>>(this.baseUrl+ 'likes',params);
+    return getPaginatedResult<Partial<Member[]>>(this.baseUrl+ 'likes',params, this.http);
    }
 
-  private getPaginatedResult<T>(url, params) {
-    const paginatedResult: PaginatedResult<T> = new PaginatedResult<T>();
-    return this.http.get<T>(url, { observe: 'response', params }).pipe(
-      map(response => {
-        paginatedResult.result = response.body;
-        if (response.headers.get('Pagination') !== null) {
-          paginatedResult.pagintaion = JSON.parse(response.headers.get('Pagination'));
-        }
-        return paginatedResult;
-      })
-    );
-  }
-
-  private getPaginationHeaders(pageNumber:  number, pageSize: number){
-    let params = new HttpParams();
-
-
-      params = params.append('pageNumber',pageNumber.toString());
-      params = params.append('pageSize',pageSize.toString());
-    
-      return params;
-  }
 
 }
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import {MessageService} from '../_services/message.service';
+
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+messages: Message[] = [];
+pagination: Pagination;
+container ='Unread';
+pageSize =5;
+pageNumber =1;
+loading = false;
 
-  constructor() { }
+  constructor(private messageService: MessageService ) { 
+
+
+  }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages(){
+    this.loading = true;
+    this.messageService.getMessages(this.pageNumber,this.pageSize,this.container).subscribe(response =>{
+    
+      this.messages = response.result;
+      this.pagination = response.pagintaion;
+      this.loading = false;
+    }
+    );
+
+  }
+
+  deleteMessage(id:number){
+    this.messageService.deleteMessage(id).subscribe(()=>{
+this.messages.splice(this.messages.findIndex(m=>m.id === id),1);
+    });
+  }
+
+
+  pageChanged(event:any){
+    this.pageNumber = event.page;
+    this.loadMessages();
   }
 
 }
